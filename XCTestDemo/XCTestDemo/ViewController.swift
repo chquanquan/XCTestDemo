0c4b534111e42dc4b6afd551fa3a1e87bf98f6ec
chquanquan <380341629@qq.com>
2019-08-18 00:54:40 +0800

    init project and add Unit Tests

---
+++ XCTestDemo/XCTestDemo/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  XCTestDemo
+//
+//  Created by chquanquan on 2019/8/17.
+//  Copyright © 2019 chquanquan. All rights reserved.
+//
+
+import UIKit
+
+fileprivate let cellIdentifier = "cellIdentifier"
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    func numberOfSectionInTableView() -> Int {
+        return 3
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return section + 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)!
+        cell.textLabel?.text = "cell number \(indexPath.row + 1)"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "section \(section + 1)"
+    }
+}
+
---
+++ XCTestDemo/XCTestDemoTests/XCTestDemoTests.swift
@@ -0,0 +1,40 @@
+//
+//  XCTestDemoTests.swift
+//  XCTestDemoTests
+//
+//  Created by chquanquan on 2019/8/17.
+//  Copyright © 2019 chquanquan. All rights reserved.
+//
+
+import XCTest
+@testable import XCTestDemo
+
+class XCTestDemoTests: XCTestCase {
+    
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+            let viewController = ViewController()
+        let sectionNumber = viewController.numberOfSectionInTableView()
+        XCTAssert(sectionNumber == 3)
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+            for index in 0..<1000 {
+                print("test: \(index)")
+            }
+        }
+    }
+
+}
